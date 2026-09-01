179a70692106949add572864a9f5cff4dd3893da
cedric_daniel_chan@dlsu.edu.ph <cedric_daniel_chan@dlsu.edu.ph>
2026-09-01 13:49:57 +0800

    test: Add a test to the setter for referenceShip

--- unit-tests/tile.test.js
+++ unit-tests/tile.test.js
@@ -1,4 +1,4 @@
-import { Tile } from "../src/model.js";
+import { Tile, Ship } from "../src/model.js";
 
 test("test the default values of a tile", () => {
   const myTile = new Tile();
@@ -11,4 +11,13 @@ test("markHit changes this.isHit to true from false", () => {
   const myTile = new Tile();
   myTile.markHit(); // this calls the method
   expect(myTile.isHit).toBeTruthy();
+});
+
+test("setting a ship reference", () => {
+  const myTile = new Tile();
+  const myShip = new Ship(5);
+  myTile.referenceShip = myShip;
+
+  expect(myTile.referenceShip).toEqual(myShip);
+  expect(myTile.isShip).toBeTruthy();
 });
